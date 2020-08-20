68e596a31c71ba8e243812ece51edcb30f29a5fd
Kevin Porras <kporras07@gmail.com>
2020-08-20 09:57:01 -0600

    Initial commit.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_mentions_suggestions do
+  name 'Redmine Mentions Suggestions plugin'
+  author 'Author name'
+  description 'Suggest assignee based on note text.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
