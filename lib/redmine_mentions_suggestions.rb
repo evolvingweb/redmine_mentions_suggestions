c98e2640201db3edc3a0b0574af398375319dc30
Kevin Porras <kporras07@gmail.com>
2021-04-21 12:45:09 -0600

    refs #22670: Use mentions suggestions user preference to alter the plugin behavior.

--- lib/redmine_mentions_suggestions.rb
+++ lib/redmine_mentions_suggestions.rb
@@ -2,8 +2,10 @@ Rails.configuration.to_prepare do
 
   IssuesController.send :helper, IssuesControllerHelper
   require_dependency 'redmine_mentions_suggestions/hooks'
-  require_dependency 'redmine_mentions_suggestions/patches/user_preference_patch'
+end
 
+require_dependency 'redmine_mentions_suggestions/patches/user_preference_patch'
+ActiveSupport::Reloader.to_prepare do
   unless UserPreference.included_modules.include?(RedmineMentionsSuggestions::Patches::UserPreferencePatch)
     UserPreference.send :prepend, RedmineMentionsSuggestions::Patches::UserPreferencePatch
   end
