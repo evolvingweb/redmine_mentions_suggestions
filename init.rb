f0e1d009573cdb50739d2a3bcc43bc5153e6f026
Burak Ikiler <123000591+burak-ikiler-ew@users.noreply.github.com>
2024-05-22 12:29:07 +0300

    Update init.rb for version upgrade

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_mentions_suggestions do
   name 'Redmine Mentions Suggestions plugin'
   author 'Evolving Web'
   description 'Suggest assignee based on note text.'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/evolvingweb/redmine_mentions_suggestions'
   author_url 'https://evolvingweb.ca'
 end
